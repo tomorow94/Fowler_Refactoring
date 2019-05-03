25e91ce213271fba89e57c196efd4f15bef4e935
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-05-03 19:36:01 +0200

    extracted FrequentRenterPoints function

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -34,4 +34,11 @@ class Rental {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1){
+            return 2;
+        }
+        return 1;
+    }
 }
