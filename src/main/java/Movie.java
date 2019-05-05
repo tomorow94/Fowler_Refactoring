efd9e944ba45edfc973dc51eaf63eb39024db514
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-05-05 21:54:07 +0200

    move pricing methods to movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -10,6 +10,33 @@ public class Movie {
         priceCode = newpriceCode;
     }
 
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1){
+            return 2;
+        }
+        return 1;
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
     public int getPriceCode() {
         return priceCode;
     }
